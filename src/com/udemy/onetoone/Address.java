bdd21afbe5fdfd4d092e45f9d74f4688b9a4f069
knowline <sealerwang@126.com>
2016-09-08 17:26:28 +0800

    Initial commit

---
+++ src/com/udemy/onetoone/Address.java
@@ -0,0 +1,69 @@
+package com.udemy.onetoone;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADDRESS_TABLE")
+public class Address {
+
+	@Id
+	@GeneratedValue
+	private int addressId;
+
+	@Column(name = "address_name")
+	private String addressName;
+
+	@Column(name = "zip_code")
+	private int zipCode;
+
+	@OneToOne
+	@JoinColumn(name = "employeeId")
+	private Employee employee;
+
+	public Address() {
+
+	}
+
+	public Address(String addressName, int zipCode) {
+		this.addressName = addressName;
+		this.zipCode = zipCode;
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddressName() {
+		return addressName;
+	}
+
+	public void setAddressName(String addressName) {
+		this.addressName = addressName;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+}
---
+++ src/com/udemy/onetoone/App.java
@@ -0,0 +1,33 @@
+package com.udemy.onetoone;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("personal_jpa");
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		Address address = new Address("Wall Street", 1123);
+
+		Employee employee = new Employee();
+		employee.setEmployeeName("Joe Smith");
+
+		employee.setAddress(address);
+		address.setEmployee(employee);
+
+		entityManager.persist(employee);
+		entityManager.persist(address);
+
+		entityManager.getTransaction().commit();
+
+		entityManager.close();
+		entityManagerFactory.close();
+
+	}
+}
